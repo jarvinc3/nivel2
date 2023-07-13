6f24420220d3b321f2382835b65846b0e415c4fc
jarvin <mangomc03@gmail.com>
2023-07-13 11:11:03 -0400

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Nav from "./components/navs";
-import Cards from "./components/cards";
+// import Cards from "./components/cards";
 import Stays from "./components/stays";
 
 
@@ -30,10 +30,33 @@ function App() {
   // Puedes ver la variable data en consola.
   console.log(data);
   return (
+
+  
     <>
       <Nav />
       <Stays />
-      <Cards />
+      <main>
+        {data.map((e, i) => {
+          return (
+            <div className="cards" key={i}>
+              <div className="imgContainer">
+                <img className="photo" src={e.photo} alt="" />
+              </div>
+                <div className="indicationsContainer">
+                <div className="typeContainer"><h2 className="superHost">{e.superHost}</h2><p className="type"> {e.type} </p><p className="bed"> {e.beds} </p></div>
+                <div className="starRting">
+                  <span id="star" className="material-symbols-outlined">star</span>
+                  <p className="rating">{e.rating}</p>
+                </div>
+              </div>
+              <div className="description">
+                <h2 className="title">{e.title}</h2>
+              </div>
+            </div>
+          )
+          })
+        }
+      </main>
     </>
   );
 }
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,5 +1,6 @@
 
-function cards() {
+
+ const cards = () => {
     
 }
 
